092f21f94abea0a999f1c4efdd0ed1bd1c16910f
SkyTeamWater <skyteamwater@qq.com>
2021-07-09 20:57:38 +0800

    格式化 使好看

--- src/C.c
+++ src/C.c
@@ -1,18 +1,28 @@
-#include<stdio.h>
-int _ICC_ratio[]={7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2};
+#include <stdio.h>
+int _ICC_ratio[] = {7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2};
 char _ICC_ID[20];
-int _ICC_Check_ID(){
-    int sum=0;
+int _ICC_Check_ID() {
+    int sum = 0;
     char last_word;
-    for(int i=0;i<17;i++) {sum+=(_ICC_ID[i]-'0')*_ICC_ratio[i];}
-    if((12-(sum%11))==10) last_word='X';
-    else if((12-(sum%11))==0) last_word='1';
-    else if((12-(sum%11))==1) last_word='0';
-    else last_word=(12-(sum%11))+'0';
-    return last_word==_ICC_ID[17];
+    for (int i = 0; i < 17; i++) {
+        sum += (_ICC_ID[i] - '0') * _ICC_ratio[i];
+    }
+    if ((12 - (sum % 11)) == 10)
+        last_word = 'X';
+    else if ((12 - (sum % 11)) == 0)
+        last_word = '1';
+    else if ((12 - (sum % 11)) == 1)
+        last_word = '0';
+    else
+        last_word = (12 - (sum % 11)) + '0';
+    return last_word == _ICC_ID[17];
 }
-int main(){
-    for(int i=0;i<18;i++) { scanf("%c",&_ICC_ID[i]); }
-    printf("%d",_ICC_Check_ID());
+int main() {
+    while (1) {
+        for (int i = 0; i < 18; i++) {
+            scanf("%c", &_ICC_ID[i]);
+        }
+        printf("%d\n", _ICC_Check_ID());
+    }
     return 0;
 }
--- src/JavaScript.js
+++ src/JavaScript.js
@@ -1,12 +1,12 @@
-function _ICC(_ICC_ID){
-    var _ICC_ratio=[7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2];
-    var sum=0,last_word;
-    for(i=0;i<17;i++){sum+=(_ICC_ID[i]-'0')*_ICC_ratio[i];}
-    if((12-(sum%11))==10) last_word='X';
-    else if((12-(sum%11))==0) last_word='1';
-    else if((12-(sum%11))==1) last_word='0';
-    else last_word=(12-(sum%11))+'0';
-    if(last_word==_ICC_ID[17]){
+function _ICC(_ICC_ID) {
+    var _ICC_ratio = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
+    var sum = 0, last_word;
+    for (i = 0; i < 17; i++) { sum += (_ICC_ID[i] - '0') * _ICC_ratio[i]; }
+    if ((12 - (sum % 11)) == 10) last_word = 'X';
+    else if ((12 - (sum % 11)) == 0) last_word = '1';
+    else if ((12 - (sum % 11)) == 1) last_word = '0';
+    else last_word = (12 - (sum % 11)) + '0';
+    if (last_word == _ICC_ID[17]) {
         document.write("<p>1</p>");
     }
     else document.write("<p>0</p>");
